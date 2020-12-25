ad2fb7ebe1b627578995c899d3e78b67c31ddeb0
lijingrou <mljr0100@163.com>
2020-12-25 08:52:32 +0800

    上传笔记

---
+++ 7.Vue/1.Vue2/45.安装脚手架/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>我是app组件{{message}}</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            message:"不能放弃"
+        }
+    }
+
+}
+</script>
+
+<style>
+div{
+    background-color: red;
+}
+</style>
