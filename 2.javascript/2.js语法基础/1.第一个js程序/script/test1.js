2e8723191cb84857f6ed90b02fbdcca382a44c0b
lijingrou <mljr0100@163.com>
2021-03-14 20:28:03 +0800

    上传笔记

--- 2.javascript/2.js语法基础/1.第一个js程序/script/test1.js
+++ 2.javascript/2.js语法基础/1.第一个js程序/script/test1.js
@@ -1,1 +1,7 @@
-console.log("dfjjc");
+document.write("<h1>浏览器环境中代码的书写位置</h1>");
+document.write("<h1>认识基本语法</h1>");
+document.write(
+  "<h1>认识输入语句、输出语句、注释：都不是ES标准，而是宿主环境规定的</h1>"
+);
+console.log("js代码同步执行");
+alert("240分钟是标准，不贪多，不偷懒");
