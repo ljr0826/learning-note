7de69eed2d86bd2d883bca78e86bca69b8cd4d4a
lijingrou <mljr0100@163.com>
2021-04-11 11:17:43 +0800

    上传笔记

--- 2.javascript/common/BetterFunction.js
+++ 2.javascript/common/BetterFunction.js
@@ -1,23 +1,23 @@
-//单对象模式
+//单对象模式，也叫做命名空间模式
 var MyFunctions = {
   /**
-   * 该函数用于判断某个数是不是奇数
-   * @param {number} n 数字
-   * @reutrns {number} 是否是奇数
+   * 判断某个数是不是奇数
+   * @param {number} n 要判断的数
+   * @returns {boolean}
    */
   isOdd: function (n) {
-    return n % 2 === 1;
+    return n % 2 !== 0;
   },
   /**
-   * 该函数用于判断某个数是不是素数
-   * @param {number} n 数字
-   * @returns {boolean} 是否是素数
+   * 判断某个数是不是素数
+   * @param {number} n 要判断的数
+   * @returns {boolean}
    */
   isPrime: function (n) {
     if (n < 2) {
       return false;
     }
-    for (var i = 2; i <= n - 1; i++) {
+    for (var i = 2; i < n; i++) {
       if (n % i === 0) {
         return false;
       }
@@ -25,9 +25,9 @@ var MyFunctions = {
     return true;
   },
   /**
-   * 该函数用于对数组求和
-   * @param {object} arr 数组
-   * @returns {number} 数组的和
+   * 对数组求和
+   * @param {object} arr 要求和的数组
+   * @returns {number}
    */
   sumOfArray: function (arr) {
     var sum = 0;
@@ -37,13 +37,16 @@ var MyFunctions = {
     return sum;
   },
   /**
-   * 该函数用于得到数组中的最大值
-   * @param {object} arr 数组
-   * @returns {number} 数组中的最大值
+   * 得到数组中的最大值
+   * @param {object} arr 要判断的数组，如果数组长度为0，返回undefined
+   * @return {number}
    */
   maxOfArray: function (arr) {
+    if (arr.length === 0) {
+      return;
+    }
     var max;
-    for (var index of arr) {
+    for (var index in arr) {
       if (!max || arr[index] > max) {
         max = arr[index];
       }
@@ -51,13 +54,16 @@ var MyFunctions = {
     return max;
   },
   /**
-   * 该函数用于得到数组中的最小值
-   * @param {object} arr 数组
-   * @returns {number} 数组中的最小值
+   * 得到数组中的最小值
+   * @param {object} arr 要判断的数组
+   * @returns {number}
    */
   minOfArray: function (arr) {
+    if (arr.length === 0) {
+      return;
+    }
     var min;
-    for (var index of arr) {
+    for (var index in arr) {
       if (!min || arr[index] < min) {
         min = arr[index];
       }
@@ -65,36 +71,39 @@ var MyFunctions = {
     return min;
   },
   /**
-   * 该函数用于判断数组是否是稀松数组
-   * @param {object} arr 数组
-   * @returns {boolean} 是否是稀松数组
+   * 判断数组是否是稀松数组
+   * @param {object} arr 要判断的数组
+   * @returns {boolean}
    */
   hasEmptyInArray: function (arr) {
-    for (var i = 0; i <= arr.length - 1; i++) {
-      if (typeof arr[i] === "undefined") {
-        //袁老师思路
-        //if(!(i in arr)){
+    //稀松数组的特点：下标是连续的
+    for (var i = 0; i < arr.length; i++) {
+      if (!(i in arr)) {
         return true;
       }
+      // if (arr[i] === undefined) {
+      //   return true;
+      // }
     }
     return false;
   },
   /**
-   * 判断该年是否是闰年
-   * @param {string} year 某年
-   * @returns {boolean} 是否是闰年
+   * 判断某年是否是闰年
+   * @param {number} year 要判断的年份
+   * @returns {boolean}
    */
-  isLeap: function (year) {
+  isLeep: function (year) {
     return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
   },
   /**
    * 得到某年某月的天数
-   * @param {string} year 某年
-   * @param {number} month 某月
+   * @param {number} year 要判断的年份
+   * @param {number} month 要判断的月份
+   * @return {number}
    */
   getDays: function (year, month) {
     if (month === 2) {
-      return this.isLeap(year) ? 29 : 28;
+      return this.isLeep(year) ? 29 : 28;
     } else if (
       (month < 8 && this.isOdd(month)) ||
       (month >= 8 && !this.isOdd(month))
@@ -106,41 +115,39 @@ var MyFunctions = {
   },
   /**
    * 得到某个数字数组中出现次数最多的数字和频率
-   * @param {object} arr 数字数组
+   * @param {object} arr 要判断的数组
+   * @return {object}
    */
   getTopFreqInArray: function (arr) {
-    var nums = {};
-    for (var prop in arr) {
-      var index = arr[prop];
-      if (!nums[index]) {
-        nums[index] = 1;
+    var obj = {};
+    for (var index in arr) {
+      if (!obj[arr[index]]) {
+        obj[arr[index]] = 1;
       } else {
-        nums[index]++;
+        obj[arr[index]]++;
       }
     }
-    var result = undefined;
-    for (var index in nums) {
-      if (!result || nums[index] > result.topFreq) {
-        result = {
-          topFreq: +nums[index],
-          topMax: index,
-        };
+    console.log(obj);
+    var result = {};
+    for (var prop in obj) {
+      if (!result.topFreq || result.topFreq < obj[prop]) {
+        result.topFreq = obj[prop];
+        result.topNum = +prop;
       }
     }
     return result;
-    //函数的返回值只能有一个
   },
   /**
    * 给指定的数组升序排序
-   * @param {*} arr
-   * @param {Function} compare 比较大小，
-   * 该函数有两个参数，代表数组中的两个元素，
-   * 该函数返回一个数字，如果是正数，则第一个元素比第二个元素大，
-   * 如果是0，则相等，
+   * @param {object} arr
+   * @param {function} compare 比较大小，
+   * 该函数有两个参数，代表数组中的两个元素，该函数返回一个数字，
+   * 如果是正数，则第一个元素比第二个元素大，
+   * 如果是0，则两个元素相等，
    * 如果是负数，则第一个元素比第二个元素小
    */
   sort: function (arr, compare) {
-    if (!compare) {
+    if (compare === undefined) {
       compare = function (a, b) {
         if (a > b) {
           return 1;
@@ -151,23 +158,21 @@ var MyFunctions = {
         }
       };
     }
-
     for (var i = 1; i < arr.length; i++) {
       for (var j = 0; j < arr.length - i; j++) {
-        //比较arr[j] 和 arr[j+1]
         if (compare(arr[j], arr[j + 1]) > 0) {
-          //交换
           var temp = arr[j];
           arr[j] = arr[j + 1];
           arr[j + 1] = temp;
         }
       }
     }
+    return arr;
   },
   /**
-   * 筛选数组
+   *
    * @param {*} arr
-   * @param {Function} callback 回调函数，接收两个参数，
+   * @param {*} callback 回调函数，接受两个参数，
    * 分别表示数组的某一项和其下标，返回boolean
    * 满足条件返回true，否则返回false
    */
@@ -182,42 +187,35 @@ var MyFunctions = {
     return newArr;
   },
   /**
-   * 从指定的数组中，查找第一个满足条件的元素，如果没有找到，返回undefined
+   *从指定的数组中查找第一个满足条件的元素，如果没有找到返回undefined
    * @param {*} arr
-   * @param {*} callback 回调函数，接收两个参数，
+   * @param {*} callback 回调函数，接受两个参数，
    * 分别表示数组的某一项和其下标，返回boolean
    * 满足条件返回true，否则返回false
+   * @returns
    */
   find: function (arr, callback) {
-    for (var i = 0; i < arr.length; i++) {
-      if (callback(arr[i], i)) {
-        return arr[i];
+    for (var index in arr) {
+      if (callback(arr[index])) {
+        return arr[index];
       }
     }
   },
   /**
    * 按照指定的条件，得到某个数组中满足条件的元素数量
    * @param {*} arr
-   * @param {*} callback 回调函数，接收两个参数，
+   * @param {*} callback 回调函数，接受两个参数，
    * 分别表示数组的某一项和其下标，返回boolean
    * 满足条件返回true，否则返回false
+   * @returns
    */
   count: function (arr, callback) {
-    var num = 0;
-    for (var i = 0; i < arr.length; i++) {
-      if (callback(arr[i], i)) {
-        num++;
+    var count = 0;
+    for (var index in arr) {
+      if (callback(arr[index])) {
+        count++;
       }
     }
-    return num;
-  },
-  /**
-   * 得到一个最小值到最大值之间的随机整数
-   * @param {number} min 最小值
-   * @param {number} max 最大值(取不到最大值)
-   */
-  //若数据为负数，将Number.parseInt()更改为向下取整Math.floor()更标准
-  getRandom: function (min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
+    return count;
   },
 };
